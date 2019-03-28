ac3a3a36aea25e6a5136e26dd08797f3736d5885
ddrag23 <kurodyami@gmail.com>
2019-03-29 06:15:27 +0700

    versi 1.01

--- application/views/dashboard/main.php
+++ application/views/dashboard/main.php
@@ -9,6 +9,7 @@
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
@@ -18,14 +19,116 @@
   }
   .about-content{
     text-align: center;
+    margin: 100px;
+    display: inline-block;
+    background-color: #777eff;
+    padding: 50px;
+    color: white;
+    box-sizing: border-box;
+    border-radius: 10px;
   }
   .headPorto{
     text-align: center;
+    padding-bottom: 20px;
   }
   .cstom{
-    overflow: auto;
-    height: 370px;
+    height: 400px;
   }
+  .color1{
+    background-color:#EE729E;
+  }
+  .color2{
+    background-color:#0c0cad;
+  }
+  .color4{
+    background-color:#07c42a;
+  }
+  .color3{
+    background-color:#3644AF;
+  }
+  nav{
+    background-color: blue;
+  }
+  .head-is{
+    margin-top: 30px;
+    margin-bottom: 30px;
+  }
+  .service-panel{
+    margin: 70px 100px 100px 100px;
+    }
+
+    .portofolio-panel {
+      margin: 30px 100px 100px 100px;
+    }
+    .container-img {
+      position: relative;
+    }
+
+    .image {
+      display: block;
+      width: 100%;
+      height: auto;
+    }
+
+    .overlay {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      height: 100%;
+      width: 100%;
+      opacity: 0;
+      transition: .5s ease;
+      background-color: black;
+    }
+
+    .container-img:hover .overlay {
+      opacity: 0.6;
+    }
+
+    .text {
+      color: white;
+      font-size: 20px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      -webkit-transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      text-align: center;
+    }
+    .btn-all{
+      background-color: pink;
+      width: 50px;
+      box-sizing: border-box;
+      border: 1px solid black;
+      float: left;
+    }
+    .page-footer{
+      background-color: #000f28;
+    }
+    .sub{
+      padding-bottom: 40px;
+    }
+    .button-btn{
+      margin: 0 0 30px 0;
+    }
+    .panel-carousel{
+      display: none;
+      background-color: black;
+      opacity: 0.5;
+    }
+    .btn-edit{
+      width: 100%;
+      cursor: pointer;
+    }
+    .contact-us{
+      margin-bottom: 100px;
+      border-radius: 30px;
+      box-sizing: content-box;
+    }
+
   </style>
   <script type="text/javascript">
 
@@ -34,6 +137,15 @@
     $('.carousel.carousel-slider').carousel({
       fullWidth: true,
       indicators: true
+    });
+    $('.carousel').mouseenter(function(){
+        $('.panel-carousel').show(function(){
+          $('panel-carousel').fadeOut(1000);
+        });
+
+    })
+    $('.carousel').mouseleave(function(){
+      $('.panel-carousel').hide();
     });
   });
   </script>
@@ -68,166 +180,157 @@
       <div class="carousel-fixed-item center">
         <a class="btn waves-effect white grey-text darken-text-2">button</a>
       </div>
-      <div class="carousel-item red white-text" href="#one!">
-        <h2>First Panel</h2>
-        <p class="white-text">This is your first panel</p>
+      <div class="carousel-item" href="#one!">
+        <img src="assets/img/car1.jpeg" alt="">
+        <h2 class="text panel-carousel">First Panel</h2>
+        <p class="text panel-carousel">This is your first panel</p>
       </div>
-      <div class="carousel-item amber white-text" href="#two!">
-        <h2>Second Panel</h2>
-        <p class="white-text">This is your second panel</p>
-      </div>
-      <div class="carousel-item green white-text" href="#three!">
-        <h2>Third Panel</h2>
-        <p class="white-text">This is your third panel</p>
-      </div>
-      <div class="carousel-item blue white-text" href="#four!">
-        <h2>Fourth Panel</h2>
-        <p class="white-text">This is your fourth panel</p>
+      <div class="carousel-item" href="#two!">
+        <img src="assets/img/car2.jpeg" alt="">
+        <h2 class="text panel-carousel">Second Panel</h2>
+        <p class="text panel-carousel">This is your second panel</p>
       </div>
     </div>
   </section>
   <!-- carousel end -->
   <!-- about-us start -->
-  <section>
-    <div class="about-content container">
-      <h2>Tentang Kami</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <section class="about-content z-depth-5">
+    <div class="row">
+      <div class="col s6 m12 l12">
+        <div class="isi-kami">
+          <h2>Tentang Kami</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
     </div>
   </section>
   <!-- about-us end -->
 
   <!-- service start -->
-  <section>
-
+  <section class="service-panel">
+    <div class="headPorto">
+      <h2>Layanan Kami</h2>
+      <p class="sub"><b>Web Development, Service Komputer, Desain Grafis, Hosting</b></p>
+    </div>
     <div class="row">
-      <div class="col s3 m3 l3">
-        <div class="card-panel center cstom">
-          <i class="small material-icons">web</i>
-          <h3>Web Development</h3>
-          <span class="text">I am a very simple card. I am good at containing small bits of information.
+      <div class="col s12 m10 l3">
+        <div class="card-panel center cstom color1">
+          <span class="">
+            <img src="http://lapakdhani.com/wp-content/uploads/2019/03/icon-service-1.png" alt="">
+          </span>
+          <h5 class="head-is white-text">Web Development</h5>
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
             I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
           </span>
         </div>
       </div>
-      <div class="col s3 m3 l3">
-        <div class="card-panel center cstom">
-          <i class="small material-icons">settings</i>
-          <h3>service Komputer</h3>
-          <span class="text">I am a very simple card. I am good at containing small bits of information.
+      <div class="col s12 m10 l3">
+        <div class="card-panel center cstom color2">
+          <span>
+            <img src="http://lapakdhani.com/wp-content/uploads/2019/03/icon-service-3.png" alt="">
+          </span>
+          <h5 class="head-is white-text">service Komputer</h5>
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
             I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
           </span>
         </div>
       </div>
-      <div class="col s3 m3 l3">
-        <div class="card-panel center cstom">
-          <i class="small material-icons">brush</i>
-          <h3>Desain Grafis</h3>
-          <span class="text">I am a very simple card. I am good at containing small bits of information.
+      <div class="col s12 m10 l3">
+        <div class="card-panel center cstom color3">
+          <span>
+            <img src="http://lapakdhani.com/wp-content/uploads/2019/03/icon-service-2.png" alt="">
+          </span>
+          <h5 class="head-is white-text">Desain Grafis</h5>
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
             I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
           </span>
         </div>
       </div>
-      <div class="col s3 m3 l3">
-        <div class="card-panel center cstom ">
-          <i class="small material-icons">domain</i>
-          <h3>Hosting</h3>
-          <span class="text">I am a very simple card. I am good at containing small bits of information.
+      <div class="col s12 m10 l3 ">
+        <div class="card-panel center cstom  color4">
+          <span>
+            <img src="assets/img/icon/hosting1.png" alt="">
+          </span>
+          <h5 class="head-is white-text">Hosting</h5>
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
             I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
           </span>
         </div>
       </div>
 
+  </div>
   </section>
-      </div>
   <!-- service end -->
 
   <!-- portofolio area start -->
-  <section>
-    <div class="portofolio container">
-      <h3 class="headPorto">portofolio</h3><br><br>
+  <section class="portofolio-panel">
+    <div class="headPorto sub">
+      <h2>portofolio</h2>
+    </div>
+    <div class="button-btn center">
+      <a class="waves-effect waves-light btn">button</a>
+      <a class="waves-effect waves-light btn">button</a>
+      <a class="waves-effect waves-light btn">button</a>
     </div>
     <div class="row">
-      <div class="col s4 m4 l4">
-        <div class="card">
-          <div class="card-image">
-            <img src="assets/img/ex1.jpg">
-            <span class="card-title black-text">Card Title</span>
-          </div>
-          <div class="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information.
-              I am convenient because I require little markup to use effectively.</p>
-            </div>
-            <div class="card-action">
-              <a href="#">Read More</a>
-            </div>
+      <div class="col s12 m10 l4">
+        <div class="container-img">
+          <img src="assets/img/ex1.jpg" alt="Avatar" class="image">
+          <div class="overlay">
+            <div class="text">semua portofolio</div>
           </div>
         </div>
-        <div class="col s4 m4 l4">
-          <div class="card">
-            <div class="card-image">
-              <img src="assets/img/ex1.jpg">
-              <span class="card-title black-text">Card Title</span>
-            </div>
-            <div class="card-content">
-              <p>I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively.</p>
-              </div>
-              <div class="card-action">
-                <a href="#">Read More</a>
-              </div>
-            </div>
+      </div>
+      <div class="col s12 m10 l4">
+        <div class="container-img">
+          <img src="assets/img/ex1.jpg" alt="Avatar" class="image">
+          <div class="overlay">
+            <div class="text">desain</div>
           </div>
-          <div class="col s4 m4 l4">
-            <div class="card">
-              <div class="card-image">
-                <img src="assets/img/ex1.jpg">
-                <span class="card-title black-text">Card Title</span>
-              </div>
-              <div class="card-content">
-                <p>I am a very simple card. I am good at containing small bits of information.
-                  I am convenient because I require little markup to use effectively.</p>
+        </div>
+      </div>
+      <div class="col s12 m10 l4">
+        <div class="container-img">
+          <img src="assets/img/ex1.jpg" alt="Avatar" class="image">
+          <div class="overlay">
+            <div class="text">web</div>
+          </div>
+        </div>
+      </div>
+    </div>
+      </section>
+
+      <!-- contact-us area start -->
+      <section>
+        <div class="container contact-us">
+          <h2 class="headPorto ">Kontak Kami</h2 ><br>
+            <div class="row">
+              <form class="col s12">
+                <div class="row">
+                  <div class="input-field col s6">
+                    <input placeholder="masukkan username anda" id="username" type="text" class="validate">
+                    <label for="username">username</label>
+                  </div>
+                  <div class="input-field col s6">
+                    <input id="email" type="email" class="validate">
+                    <label for="email">Email</label>
+                  </div>
                 </div>
-                <div class="card-action">
-                  <a href="#">Read More</a>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <input placeholder="tulis pesan" id="tulisPesan" type="text" class="validate">
+                    <label for="tulisPesan">Pesan</label>
+                  </div>
                 </div>
-              </div>
+                <button class="btn waves-effect waves-light btn-edit" type="submit" name="action">Submit
+                  <i class="material-icons right">send</i>
+                </button>
+              </form>
             </div>
           </div>
         </section>
-        <!-- portofolio area end -->
+        <!-- contact-us area end -->
 
-        <!-- contact-us area start -->
-        <section>
-          <div class="container contact-us">
-            <h4 class="headPorto">Kontak Kami</h4 ><br>
-              <div class="row">
-                <form class="col s12 m12 l12">
-                  <div class="row">
-                    <div class="input-field col s12">
-                      <input placeholder="Placeholder" id="first_name" type="text" class="validate">
-                      <label for="first_name">username</label>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="input-field col s12">
-                      <input id="email" type="email" class="validate">
-                      <label for="email">Email</label>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="input-field col s12">
-                      <input disabled value="tulis pesan kamu" id="disabled" type="text" class="validate">
-                      <label for="disabled">pesan</label>
-                    </div>
-                  </div>
-                </form>
-                <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-                  <i class="material-icons right">send</i>
-                </button>
-              </div>
-            </div>
-          </section>
-          <!-- contact-us area end -->
           <!-- footer area start -->
           <section>
             <footer class="page-footer">
